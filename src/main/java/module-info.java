47d2905c64adaa2a5903e682d6a55b5acbafdf64
azertysarah <92745305+azertysarah@users.noreply.github.com>
2022-05-20 10:48:49 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ricochetrobot_botuli_niane_valery {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ricochetrobot_botuli_niane_valery to javafx.fxml;
+    exports com.example.ricochetrobot_botuli_niane_valery;
+}
