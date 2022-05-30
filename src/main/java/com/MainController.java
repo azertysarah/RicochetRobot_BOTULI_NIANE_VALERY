826826e26d104cb6ceff94f8d3fae0e37451c2af
azertysarah <92745305+azertysarah@users.noreply.github.com>
2022-05-30 17:51:52 +0200

    Updates: Afficher les cases du milieu
    Prblm:
    La taille d'affichage de la fenêtre dès l'ouverture est trop petite

--- src/main/java/com/MainController.java
+++ src/main/java/com/MainController.java
@@ -18,11 +18,20 @@ public class MainController {
     public void playGame() throws Exception{
 
         //Afficher les cases du plateau dans chaque cellule de la grille
-        Image cellImage = new Image("cell.png", CELL_SIZE, CELL_SIZE, false, true);
+        Image cellImage = new Image("Cell.png", CELL_SIZE, CELL_SIZE, false, true);             //Case du plateau
+        Image emptyCellImage = new Image("EmptyCell.png", CELL_SIZE, CELL_SIZE, false, true);   //Case du milieu
+
         for (int row = 0; row < 16; row++) {
             for (int column = 0; column < 16; column++) {
                 ImageView cellImageView = new ImageView(cellImage);
                 gridBoard.add(cellImageView, row, column);
+                //Cases du milieu du plateau
+                if (row == 7 || row == 8){
+                    if (column == 7 || column == 8){
+                        ImageView emptyCellImageView = new ImageView(emptyCellImage);
+                        gridBoard.add(emptyCellImageView, row, column);
+                    }
+                }
             }
         }
 
